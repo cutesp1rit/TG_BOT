90f47f7042fc997f890d48103bdb8145ee5da043
Timofey Maksimov <tsmaksimov@edu.hse.ru>
2024-07-21 14:07:56 +0300

    Начало создания чека

--- handlers.py
+++ handlers.py
@@ -3,6 +3,7 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
 from app.pyrogram_functions import get_chat_members
 from app.data_bot import bot_token
+from user import User
 
 bot = Bot(token=bot_token)
 router = Router()
@@ -14,13 +15,19 @@ async def check_data(message: Message):
         dict_chats[id] = list()
         chat_members = await get_chat_members(id)
         # убираем бота из списка пользователей
-        dict_chats[id] = [member for member in chat_members if member != "produc1_manager_bot"]
+        chat_without_bot = [member for member in chat_members if member != "produc1_manager_bot"]
+        dict_chats[id] = [User(member, chat_without_bot) for member in chat_without_bot]
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await check_data(message)
     await message.reply("привет, сейчас я расскажу, как работает бот...")
+
+@router.message(Command('download_cheque'))
+async def cmd_new_list(message: Message):
+    await check_data(message)
+    
 
 @router.message(Command('new_list'))
 async def cmd_new_list(message: Message):
