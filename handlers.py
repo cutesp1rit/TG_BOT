133022b07c3ce6a2248956b17292f5026bc59f9d
Timofey Maksimov <tsmaksimov@edu.hse.ru>
2024-07-22 15:47:10 +0300

    Проблемы с pyrogram, сделана реализация по вводу данных в чек.

--- handlers.py
+++ handlers.py
@@ -3,7 +3,11 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
 from app.pyrogram_functions import get_chat_members
 from app.data_bot import bot_token
-from user import User
+from app.user import User
+from app.states import DownloadCheque
+from aiogram.fsm.context import FSMContext
+import pandas as pd
+
 
 bot = Bot(token=bot_token)
 router = Router()
@@ -21,22 +25,56 @@ async def check_data(message: Message):
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await check_data(message)
-    await message.reply("привет, сейчас я расскажу, как работает бот...")
+    # await check_data(message)
+    await message.reply("Привет, сейчас я расскажу, как работает бот...")
+
 
 @router.message(Command('download_cheque'))
-async def cmd_new_list(message: Message):
-    await check_data(message)
-    
+async def cmd_new_list(message: Message, state: FSMContext):
+    # await check_data(message)
+    await message.answer("Напишите количество позиций, которое будет в чеке:")
+    await state.set_state(DownloadCheque.count_of_positions)
+
+@router.message(DownloadCheque.count_of_positions)
+async def cmd_new_list(message: Message, state: FSMContext):
+    try:
+        await state.update_data(count_of_positions=int(message.text))
+        await state.set_state(DownloadCheque.product)
+        await message.reply(f"Отлично! Теперь введите название продукта №{DownloadCheque.flag + 1}:")
+    except Exception:
+        await message.reply("Вы ввели некорректные данные, пожалуйста, введите целое число, без лишних символов.")
+
+@router.message(DownloadCheque.product)
+async def cmd_new_list(message: Message, state: FSMContext):
+    await state.update_data(product=message.text)
+    await state.set_state(DownloadCheque.price)
+    await message.reply(f"Отлично! Теперь введите цену продукта №{DownloadCheque.flag + 1}:")
+
+@router.message(DownloadCheque.price)
+async def cmd_new_list(message: Message, state: FSMContext):
+    try:
+        data = await state.get_data()
+        DownloadCheque.products[data["product"]] = int(message.text)
+        await state.set_state(DownloadCheque.product)
+        DownloadCheque.flag += 1
+        if (DownloadCheque.flag == int(data["count_of_positions"])):
+            await state.set_state(DownloadCheque.finish)
+            return
+        await message.reply(f"Отлично! Теперь введите название продукта №{DownloadCheque.flag + 1}:")
+    except Exception:
+        await message.reply("Вы ввели некорректные данные, пожалуйста, введите целое число, без лишних символов.")
 
 @router.message(Command('new_list'))
 async def cmd_new_list(message: Message):
-    await check_data(message)
+    pass
+    # await check_data(message)
 
 @router.message(Command('remove_list'))
 async def cmd_remove_list(message: Message):
-    await check_data(message)
+    pass
+    # await check_data(message)
 
 @router.message()
 async def universal(message: Message):
-    await check_data(message)
+    pass
+    # await check_data(message)
--- states.py
+++ states.py
@@ -1,6 +1,10 @@
 from aiogram.fsm.state import State, StatesGroup
 
-class Test(StatesGroup):
-    first = State()
-    second = State()
-    third = State()
+class DownloadCheque(StatesGroup):
+    flag = 0
+    products = dict()
+
+    count_of_positions = State()
+    product = State()
+    price = State()
+    finish = State()
