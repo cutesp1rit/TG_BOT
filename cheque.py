b7a92a8ade5fa75eb152223bf1b36f6575c6d7aa
Timofey Maksimov <tsmaksimov@edu.hse.ru>
2024-07-24 17:08:05 +0300

    Еще пока кривой вывод, но чек доделан! Написал еще заметки в хэндлерах, что нужно изменить

--- cheque.py
+++ cheque.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Cheque:
     def __init__(self, users):
         self.users_ = users
@@ -6,8 +8,17 @@ class Cheque:
         for user in users:
             self.dict_money_[str(user)] = list()
 
-    def new_product(self, product, price, users):
+    async def new_product(self, product, price, users):
         self.products_.append(product)
         divided_price = price / len(users)
         for user in users:
             self.dict_money_[str(user)].append(divided_price)
+        for user in self.users_:
+            if not (user in users):
+                self.dict_money_[str(user)].append(0)
+
+    async def make_cheque(self):
+        self.cheque_ = pd.DataFrame(self.dict_money_)
+        self.cheque_.index = self.products_
+        print(self.cheque_)
+        return str(self.cheque_)
--- handlers.py
+++ handlers.py
@@ -7,12 +7,16 @@ from app.user import User
 from app.chat import Chat
 from app.states import DownloadCheque
 from aiogram.fsm.context import FSMContext
+from app.cheque import Cheque
 import app.keyboards as kb
-import pandas as pd
 
 
 # переименовать методы
 # какие методы должны быть async
+# поля
+# скрытие кнопок
+# округление чисел
+# красивый чек
 
 bot = Bot(token=bot_token)
 router = Router()
@@ -45,12 +49,14 @@ async def cmd_start(message: Message):
 @router.message(Command('download_cheque'))
 async def cmd_download_cheque(message: Message, state: FSMContext):
     # await check_data(message)
+    curr_chat : Chat = dict_chats[message.chat.id]
+    curr_chat.last_cheque_ = Cheque(curr_chat.get_users())
     await message.reply("Напишите количество позиций, которое будет в чеке:")
     await state.set_state(DownloadCheque.count_of_positions)
 
 @router.message(DownloadCheque.count_of_positions)
 async def cmd_new_list(message: Message, state: FSMContext):
-    curr_chat = dict_chats[message.chat.id]
+    curr_chat : Chat = dict_chats[message.chat.id]
     try:
         await state.update_data(count_of_positions=int(message.text))
         curr_chat.count_pos = int(message.text)
@@ -61,14 +67,14 @@ async def cmd_new_list(message: Message, state: FSMContext):
 
 @router.message(DownloadCheque.product)
 async def cmd_new_list(message: Message, state: FSMContext):
-    curr_chat = dict_chats[message.chat.id]
+    curr_chat : Chat = dict_chats[message.chat.id]
     await state.update_data(product=message.text)
     await state.set_state(DownloadCheque.price)
     await message.reply(f"Отлично! Теперь введите цену продукта №{curr_chat.flag_main + 1}:")
 
 @router.message(DownloadCheque.price)
 async def cmd_new_list(message: Message, state: FSMContext):
-    curr_chat = dict_chats[message.chat.id]
+    curr_chat : Chat = dict_chats[message.chat.id]
     try:
         await state.update_data(price=int(message.text))
         await state.set_state(DownloadCheque.num_people)
@@ -79,7 +85,7 @@ async def cmd_new_list(message: Message, state: FSMContext):
 
 @router.message(DownloadCheque.num_people)
 async def cmd_new_list(message: Message, state: FSMContext):
-    curr_chat = dict_chats[message.chat.id]
+    curr_chat : Chat = dict_chats[message.chat.id]
     try:
         curr_chat.count_user = int(message.text)
         await state.set_state(DownloadCheque.person)
@@ -90,7 +96,8 @@ async def cmd_new_list(message: Message, state: FSMContext):
 
 @router.message(DownloadCheque.person)
 async def cmd_new_list(message: Message, state: FSMContext):
-    curr_chat = dict_chats[message.chat.id]
+    data = await state.get_data()
+    curr_chat : Chat = dict_chats[message.chat.id]
     try:
         if not (message.text in curr_chat.get_users()):
             raise Exception
@@ -99,10 +106,13 @@ async def cmd_new_list(message: Message, state: FSMContext):
         if (curr_chat.flag_user == curr_chat.count_user):
             curr_chat.flag_user = 0
             curr_chat.flag_main += 1
+            await curr_chat.last_cheque_.new_product(data["product"], int(data["price"]), curr_chat.list_users_products.copy())
+            curr_chat.list_users_products = list()
             if curr_chat.flag_main == curr_chat.count_pos:
                 await curr_chat.reset()
                 await state.clear()
                 await message.reply(f"Ура! Мы получили все данные и уже составляем ваш чек..")
+                await message.answer("```" + await curr_chat.last_cheque_.make_cheque() + "```")
                 return
                 # передаем данные в метод
             await state.set_state(DownloadCheque.product)
